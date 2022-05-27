df50f6ef644d2dbde5aa7d37905a86d2e1b191e2
Artist <anonymousartistvvs@protonmail.com>
2022-05-26 20:09:20 -0400

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,3 @@
+n = int(input('Which number? ')) #Asking the user to input a number
+for i in range(n): #The range gives back a series of numbers thats leading up to that number. For example, if you input 5 you will get back 0,1,2,3,4. So i will be the variable for range
+        print(i**2) #Print out the square root of the ranged number.
