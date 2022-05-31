da40abecbfb0d1745a1060fd1a760396e9929c82
Artist <anonymousartistvvs@protonmail.com>
2022-05-31 10:35:04 -0400

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,21 @@
+fruit = input("Fruit: ")
+noun = input("Noun: ")
+name = input("Name: ")
+adj = input("Adjective: ")
+colour = input("Color: ")
+adj1 = input("Second Adjective: ")
+adj2 = input("Thrid Adjective: ")
+noun1 = input("Secound Noun: ")
+verb = input("Verb: ")
+noun2 = input("Second Noun: ")
+verb1 = input("Second Verb: ")
+animal = input("Animal: ")
+
+madlib = f"Ohhhhh... who lives in a {fruit} under the {noun}? \n {name} Squarepants! \n {adj} and {colour} and {adj1} is he! \n {name} Squarepants! \n If {adj2} be somthing you {noun1}... \n {name} Squarepants! \n Then {verb} on the {noun2} and {verb1} like a {animal}. \n {name} Squarepants! \n Ready? \n {name} Squarepants! \n {name} Squarepants! \n {name} Squarepants! \n {name}... Squarepants! ahhhahhhahahhaha!"
+
+print(madlib)
+
+#In this project I give the user a chance to choose he/she words they would like to use in this spongebob theme song madlib.
+#Instead of using concatenation or commas I choose to use the f string to get into the habit of using it.
+#I also used the new line to make the code more readable when I run it.
+#It's pretty easy for me now but when time goes on and I undergo more intermediate projects I'll be using stackoverflow for help.
